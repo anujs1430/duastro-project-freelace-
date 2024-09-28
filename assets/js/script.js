bf0379cb721a8f83f6778fc45d40c1803474b69e
anuj singh <anujs1430>
2024-09-28 17:48:50 +0530

    payment-success, login-signup page done, freekundali form popup done, service page bottom button done, payment success page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -281,3 +281,75 @@ $(document).ready(function () {
     $(".category-nav-items").fadeToggle();
   });
 });
+//
+//
+//
+//
+//
+//
+//
+// ===========Log in sign up form
+let flag = 0;
+
+$("#name").hide();
+$("#confirmPass").hide();
+$(".signin").on("click", function () {
+  if (flag == 0) {
+    $(".move").addClass("moving");
+    $(".move").removeClass("start");
+
+    $(".form").addClass("movingForm");
+    $(".form").removeClass("startForm");
+
+    $(".hello").show();
+    $(".welcome").hide();
+
+    $(".move").css("background-position", "right");
+
+    setTimeout(function () {
+      $(".title").text("Register Here");
+      $(".light").text("Or use your email account");
+      $(".name").hide();
+      $(".p-button").text("LOG IN");
+      $(".b-button").text("SIGN UP");
+      $(".forgot").show();
+      $(".form").css("border-radius", "10px 0px 0px 10px");
+      $(".move").css("border-radius", "0px 10px 10px 0px");
+      $("#name").show();
+      $("#confirmPass").show();
+      $(".icons ").css("marginBottom", "10px");
+      $(".form-logo ").css("left", "0");
+    }, 200);
+
+    flag = 1;
+  } else {
+    $(".move").removeClass("moving");
+    $(".move").addClass("start");
+
+    $(".form").removeClass("movingForm");
+    $(".form").addClass("startForm");
+
+    $(".hello").hide();
+    $(".welcome").show();
+
+    $(".move").css("background-position", "left");
+
+    setTimeout(function () {
+      $(".title").text("Create Account");
+      $(".light").text("Or use your email for registration");
+      $(".name").show();
+      $(".p-button").text("SIGN UP");
+      $(".b-button").text("LOG IN");
+      $(".forgot").hide();
+      $(".form").css("border-radius", "0px 10px 10px 0px");
+      $(".move").css("border-radius", "10px 0px 0px 10px");
+      $(".icons ").css("marginBottom", "3rem");
+      $("#name").hide();
+      $("#confirmPass").hide();
+      $(".form-logo ").css("left", "");
+    }, 200);
+
+    flag = 0;
+  }
+});
+// ===========Log in sign up form
